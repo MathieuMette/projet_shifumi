2995cd84b774e74aa2c08a589aceb1aaa8964412
MathieuMette <mathieu.mette@gmail.com>
2022-05-05 14:40:33 +0200

    maj

---
+++ assets/js/game.js
@@ -0,0 +1,6 @@
+var sheetUser = document.getElementById('sheetUser');
+
+sheetUser.addEventListener('click', function(){
+    var feuilleUser = document.getElementById('feuilleUser');
+    feuilleUser.classList.add('userActive')
+})
