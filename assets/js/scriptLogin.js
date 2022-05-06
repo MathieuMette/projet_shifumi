b5dfa938d567ad2689e7ef9346911b89ae529e1f
Anais258 <anaisguyon@outlook.com>
2022-05-06 09:36:59 +0200

    test push js

--- assets/js/scriptLogin.js
+++ assets/js/scriptLogin.js
@@ -4,14 +4,17 @@
 // var man1 = document.querySelector('#man1')
 // var man2 = document.querySelector('#man2')
 // var man3 = document.querySelector('#man3')
-// var avatars = document.querySelectorAll('.avatar')
+// var avatar = document.getElementById('man3')
 // var avatar = document.querySelector('.avatar')
+
+
 
 // for (avatar of avatars) {
 //         avatar.addEventListener('click', function(avatar) {
 //         console.log('test')
 //         this.style.visibility="hidden";
 // })}
+
 
 
 // function showAndHide () {
@@ -23,13 +26,22 @@
 //                     avatar.style.display=='none'
 //                 }
 //         }
-        
-// avatar.onclick = function() {
-//     console.log('test')
-// }
 
-    // avatar.classList.add('active')
-    // avatars.style.visibility="hidden"
+// console.log('test');
+// var avatar = document.querySelector('#man1')
+// avatar.classList.add('active')
+// console.log(avatar)
 
 
+// var avatars = document.querySelectorAll(".avatar");
+// var woman1 = document.querySelector("woman1");
+// var woman2 = document.querySelector("#woman2");
+// var woman3 = document.querySelector("#woman3");
+// var man1 = document.querySelector("#man1");
+// var man2 = document.querySelector("#man2");
+// var man3 = document.querySelector("#man3");
+// woman1.addEventListener('click', function() {
+//     if (woman1="onclick") {
+//         woman1.classList.add('active');
+//     }})
 
