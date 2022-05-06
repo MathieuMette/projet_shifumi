d9cda850603c4f66a3a3b92c47b37ceb8c37531f
Anais258 <anaisguyon@outlook.com>
2022-05-06 11:57:34 +0200

    ajout logo égalité

--- assets/js/scriptLogin.js
+++ assets/js/scriptLogin.js
@@ -1,37 +1,18 @@
+var avatars = document.querySelector(".listAvatar");
+var woman1 = document.querySelector("#woman1");
+var woman2 = document.querySelector("#woman2");
+var woman3 = document.querySelector("#woman3");
+var man1 = document.querySelector("#man1");
+var man2 = document.querySelector("#man2");
+var man3 = document.querySelector("#man3");
 
-// for (avatar of avatars) {
-//         avatar.addEventListener('click', function(avatar) {
-//         console.log('test')
-//         this.style.visibility="hidden";
-// })}
-
-
-
-// function showAndHide () {
-//         if (avatar.onclick) {
-//                 console.log('test')
-//                 avatar.style.display=='block'
-//             } else {
-//                     console.log('test2')
-//                     avatar.style.display=='none'
-//                 }
-//         }
-
-// console.log('test');
-// var avatar = document.querySelector('#man1')
-// avatar.classList.add('active')
-// console.log(avatar)
-
-
-// var avatars = document.querySelectorAll(".avatar");
-// var woman1 = document.querySelector("woman1");
-// var woman2 = document.querySelector("#woman2");
-// var woman3 = document.querySelector("#woman3");
-// var man1 = document.querySelector("#man1");
-// var man2 = document.querySelector("#man2");
-// var man3 = document.querySelector("#man3");
-// woman1.addEventListener('click', function() {
-//     if (woman1="onclick") {
-//         woman1.classList.add('active');
-//     }})
+function chooseAvatar () {
+    if (avatars="onclick") {
+        woman1.addEventListener('click', function() {
+        woman1.classList.add('.active');
+    //     })
+    // } else {
+    //     woman1.classList.add('.inactive');
+    // }
+}
 
