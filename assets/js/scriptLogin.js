3fb607246aef24c851dcbf26575b26f586c78238
Anais258 <anaisguyon@outlook.com>
2022-05-06 15:38:59 +0200

    maj

--- assets/js/scriptLogin.js
+++ assets/js/scriptLogin.js
@@ -1,11 +1,10 @@
-var avatars = document.querySelectorAll(".avatar");
-
 var woman1 = document.querySelector("#woman1");
 var woman2 = document.querySelector("#woman2");
 var woman3 = document.querySelector("#woman3");
 var man1 = document.querySelector("#man1");
 var man2 = document.querySelector("#man2");
 var man3 = document.querySelector("#man3");
+var sourceAvatar = ('');
 
 woman1.addEventListener('click', function() {
     woman1.classList.add('active');
@@ -14,7 +13,9 @@ woman1.addEventListener('click', function() {
     man1.classList.remove('active');
     man2.classList.remove('active');
     man3.classList.remove('active');
+    sourceAvatar = "assets/img/avatars_game/avatar_magali.png";
 })
+
 woman2.addEventListener('click', function() {
     woman1.classList.remove('active');
     woman2.classList.add('active');
@@ -22,7 +23,9 @@ woman2.addEventListener('click', function() {
     man1.classList.remove('active');
     man2.classList.remove('active');
     man3.classList.remove('active');
+    sourceAvatar = "assets/img/avatars_game/avatar_imelda.png";
 })
+
 woman3.addEventListener('click', function() {
     woman1.classList.remove('active');
     woman2.classList.remove('active');
@@ -30,7 +33,9 @@ woman3.addEventListener('click', function() {
     man1.classList.remove('active');
     man2.classList.remove('active');
     man3.classList.remove('active');
+    sourceAvatar = "assets/img/avatars_game/avatar_michele.png";
 })
+
 man1.addEventListener('click', function() {
     woman1.classList.remove('active');
     woman2.classList.remove('active');
@@ -38,7 +43,9 @@ man1.addEventListener('click', function() {
     man1.classList.add('active');
     man2.classList.remove('active');
     man3.classList.remove('active');
+    sourceAvatar = "assets/img/avatars_game/avatar_thomas.png";
 })
+
 man2.addEventListener('click', function() {
     woman1.classList.remove('active');
     woman2.classList.remove('active');
@@ -46,7 +53,9 @@ man2.addEventListener('click', function() {
     man1.classList.remove('active');
     man2.classList.add('active');
     man3.classList.remove('active');
+    sourceAvatar = "assets/img/avatars_game/avatar_ines.png";
 })
+
 man3.addEventListener('click', function() {
     woman1.classList.remove('active');
     woman2.classList.remove('active');
@@ -54,6 +63,25 @@ man3.addEventListener('click', function() {
     man1.classList.remove('active');
     man2.classList.remove('active');
     man3.classList.add('active');
+    sourceAvatar = "assets/img/avatars_game/avatar_demphis.png";
 })
 
 
+// récupération des pseudos et avatar
+
+
+
+
+var go = document.getElementById('ok');
+
+go.addEventListener('click', function() {
+    console.log('test')
+    var choosePseudo = document.getElementById('choosePseudo').value;
+    if (choosePseudo == '') {
+        alert('Please remplir le pseudo')    
+    } else if (sourceAvatar == '') {
+        alert('Please choisir un avatar')
+    } else {
+        
+    }
+})
