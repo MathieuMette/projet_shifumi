a7e370b3d5cb52be5bd91cc6607de0b52ba21a16
Anais258 <anaisguyon@outlook.com>
2022-05-06 11:30:31 +0200

    mains qui trournent

--- assets/js/scriptLogin.js
+++ assets/js/scriptLogin.js
@@ -1,13 +1,3 @@
-// var woman1 = document.querySelector('#woman1')
-// var woman2 = document.querySelector('#woman2')
-// var woman3 = document.querySelector('#woman3')
-// var man1 = document.querySelector('#man1')
-// var man2 = document.querySelector('#man2')
-// var man3 = document.querySelector('#man3')
-// var avatar = document.getElementById('man3')
-// var avatar = document.querySelector('.avatar')
-
-
 
 // for (avatar of avatars) {
 //         avatar.addEventListener('click', function(avatar) {
