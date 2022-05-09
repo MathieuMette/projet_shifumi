b0222e54acec41e2962a87c5b885f42cf41d99fc
Anais258 <anaisguyon@outlook.com>
2022-05-09 16:49:46 +0200

    MAJ des alertes pseudo et avatar

--- assets/js/login.js
+++ assets/js/login.js
@@ -74,14 +74,22 @@ var go = document.getElementById('ok');
 go.addEventListener('click', function() {
     var choosePseudo = document.getElementById('choosePseudo').value;
     var alertPseudo = document.querySelector('.alertPseudo');    
+    var alertAvatar = document.querySelector('.alertAvatar');    
+    var clickEndPseudo = document.querySelector('.btnPseudo');
+    var clickEndAvatar = document.querySelector('.btnAvatar');
     if (choosePseudo == '') {
-        alert('Please renseigner un pseudo')
+        alertPseudo.style.display = 'flex';
+        clickEndPseudo.addEventListener('click', function() {
+            alertPseudo.style.display = 'none';
+        })
+
     } else if (sourceAvatar == '') {
-        alert('Please choisir un avatar')
+        alertAvatar.style.display = 'flex';
+        clickEndAvatar.addEventListener('click', function() {
+            alertAvatar.style.display = 'none';
+        })
     } else {
         localStorage.setItem('choosePseudo', choosePseudo);
         localStorage.setItem('sourceAvatar', sourceAvatar);
         window.location.href="game.html";
     }})
-
-    // alertPseudo.style.display = 'flex';
